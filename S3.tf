ccea6dee57b90289934bbd12742ca43be3921dd8
RoadKing159753 <111482881+RoadKing159753@users.noreply.github.com>
2023-01-18 11:14:52 +0530

    Update S3.tf

--- S3.tf
+++ S3.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "unrestricted" {
   bucket = "examplebuckettftest"
-  #zpc-skip-policy: ZS-AWS-00026:testing
+  #zpc-skip-policy: ZS-AWS-00026,ZS-AWS-00025,ZS-AWS-00034,ZS-AWS-00037
   acl    = "public-read-write"
   versioning {
     enabled = true
